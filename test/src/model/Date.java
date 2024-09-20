4c1028b3a7886f3989075690f44f65bf8c888e4d
Chi <hoangthilinhchi131@gmail.com>
2024-09-20 08:42:13 +0700

    add Date and Time

---
+++ test/src/model/Date.java
@@ -0,0 +1,13 @@
+package model; 
+public class Date {
+    private int day;
+    private int month;
+    private int year;
+
+    public Date(int day, int month, int year) {
+        this.day = day;
+        this.month = month;
+        this.year = year;
+    }
+}
+    
--- test/src/model/OtherUser.java
+++ test/src/model/OtherUser.java
@@ -1,8 +1,6 @@
 package model;
-
 public class OtherUser extends User {
-    private int limit = 1;
-
+    private final int limit = 1;
     public OtherUser(String id, String roles, String username, String password) {
         super(id, roles, username, password);
     }
