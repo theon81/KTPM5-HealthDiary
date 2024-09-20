f00025f81cc6dbcf616492dcef7a2b99134b447f
Quixxo <enmeicchi2424@gmail.com>
2024-09-20 10:40:57 +0700

    View & CSS

--- health_diary/src/main/java/com/app/Test.java
+++ health_diary/src/main/java/com/app/Test.java
@@ -1,5 +1,7 @@
 package com.app;
 
 public class Test {
-    
+    public static void main(String[] args) {
+        
+    }
 }
---
+++ health_diary/src/main/java/com/app/view/UserApp.java
@@ -0,0 +1,13 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class UserApp extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+}
