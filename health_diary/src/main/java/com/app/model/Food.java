d68aaa07cdf884b50671eb30d7b38aaac2c03c09
ManhT1en <scvgden@gmail.com>
2024-10-01 17:51:24 +0700

    update models

---
+++ health_diary/src/main/java/com/app/model/Food.java
@@ -0,0 +1,52 @@
+package com.app.model;
+
+import com.app.model.exception.DataException;
+
+public class Food {
+    private String nameFood;
+    private String typeFood;
+    private double weightLimit;
+
+    public Food() {}
+
+    public Food(String nameFood, String typeFood, double weightLimit) {
+        this.nameFood = nameFood;
+        this.typeFood = typeFood;
+        this.weightLimit = weightLimit;
+    }
+
+    public String getNameFood() {
+        return nameFood;
+    }
+
+    public String getTypeFood() {
+        return typeFood;
+    }
+
+    public double getWeightLimit() {
+        return weightLimit;
+    }
+
+    public void setNameFood(String nameFood) {
+        this.nameFood = nameFood;
+    }
+
+    public void setTypeFood(String typeFood) {
+        this.typeFood = typeFood;
+    }
+
+    public void setWeightLimit(double weightLimit) throws DataException {
+        this.weightLimit = weightLimit;
+        if (weightLimit < 0 || weightLimit > 1000){
+            throw new DataException();
+        }
+    }
+    
+    public String getFood(){
+        return (this.getNameFood()+ "</f>" +
+                this.getTypeFood() + "</f>" +
+                this.getWeightLimit() +"</f>");
+    }
+    
+    
+}
