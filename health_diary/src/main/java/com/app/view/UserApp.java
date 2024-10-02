b4379bb5844d5a3b3f2a3b4d4dfdf11df2688502
kieutung <kieuthanhtung0502@gmail.com>
2024-10-02 10:18:04 +0700

    fix bug version

--- health_diary/src/main/java/com/app/view/UserApp.java
+++ health_diary/src/main/java/com/app/view/UserApp.java
@@ -1,33 +1,20 @@
 package com.app.view;
 
+
 import com.app.controller.UserController;
 import com.app.model.User;
+
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+
 public class UserApp extends Application {
 
     @Override
-    public void start(Stage primaryStage) {
-        primaryStage.setTitle("Login");
-
-        // Create the User object and UserController
-        User user = new User();
-        user.setID("001");
-        user.setRoles("Admin");
-        user.setUsername("user1");
-        user.setPassword("pass123");
-
-        UserController userController = new UserController(user);
-
-        // Display the login scene
-        Scene loginScene = userController.createLoginScene(primaryStage);
-        primaryStage.setScene(loginScene);
-        primaryStage.show();
+    public void start(Stage arg0) throws Exception {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'start'");
     }
 
-    public static void main(String[] args) {
-        launch(args);
-    }
 }
--- health_diary/src/main/java/module-info.java
+++ health_diary/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.app {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.graphics;
 
     opens com.app to javafx.fxml;
     exports com.app;
